0743faec2bf9acca930a27a46990eb039a671b37
astefanov9 <aleksandar.stefanov@dxc.com>
2023-08-15 12:13:41 +0300

    added new exercise

---
+++ WpfExercise/WpfExercise/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Sum SumObj { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SumObj = new Sum { Num1 = "1", Num2 = "3" };
+            this.DataContext= SumObj;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thanks for clicking me!");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse button went Up! -> Bubbling event.");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Previus mouse button went Up! -> Tunnling event.");
+        }
+    }
+}
---
+++ WpfExercise/WpfExercise/Sum.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExercise
+{
+    public class Sum : INotifyPropertyChanged
+    {
+        private string num1;
+
+        private string num2;
+
+        private string result;
+
+        public string Num1 
+        { 
+            get
+            {
+                return num1;
+            } 
+            set
+            {
+                if (int.TryParse(value, out int num))
+                {
+                    num1 = value;
+                    OnPropertyChange("Num1");
+                    OnPropertyChange("Result");
+                }
+            }
+        }
+
+        public string Num2
+        {
+            get
+            {
+                return num2;
+            }
+            set
+            {
+                if (int.TryParse(value, out int num))
+                {
+                    num2 = value;
+                    OnPropertyChange("Num2");
+                    OnPropertyChange("Result");
+                }
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                int result = int.Parse(Num1) + int.Parse(Num2);
+
+                return result.ToString();
+            }
+            set
+            {
+                int res = int.Parse(Num1) + int.Parse(Num2);
+
+                result = res.ToString();
+
+                OnPropertyChange("Result");
+            }
+        }               
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChange(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}

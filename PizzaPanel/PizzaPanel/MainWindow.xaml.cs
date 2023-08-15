088d548d7cbad636bff391e8ea8703aeef6530f5
astefanov9 <aleksandar.stefanov@dxc.com>
2023-08-15 16:21:11 +0300

    added new exercise

---
+++ PizzaPanel/PizzaPanel/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AllChekedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVall = allTopings.IsChecked == true;
+            Salami.IsChecked = newVall;
+            Mozzarella.IsChecked = newVall;
+            Mushrooms.IsChecked = newVall;
+        }
+
+        private void SingleCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            allTopings.IsChecked = null;
+
+            if (Salami.IsChecked == true && Mushrooms.IsChecked == true && Mozzarella.IsChecked == true)
+            {
+                allTopings.IsChecked = true;
+            }
+
+            if (Salami.IsChecked == false && Mushrooms.IsChecked == false && Mozzarella.IsChecked == false)
+            {
+                allTopings.IsChecked = false;
+            }
+        }
+        
+    }
+}

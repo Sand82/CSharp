ac634f87db574a3fe522165f2da65d08af6e4522
Sand82 <sandoki@abv.bg>
2023-09-04 14:45:36 +0300

    refactoring program

---
+++ SeaChess/SeaChess/Game.cs
@@ -0,0 +1,105 @@
+﻿using static SeaChess.Program;
+
+namespace SeaChess
+{   
+    public class Game
+    {
+        private Board? board;
+
+        private Player? playerOne;
+
+        private Player? playerTwo;
+
+        public Game(Player playerOne, Player playerTwo)
+        {
+            this.board = new Board();
+            this.playerOne = playerOne;
+            this.playerTwo = playerTwo;
+        }
+
+        public void Run()
+        {                    
+
+            var boardCounter = 1;
+
+            var IsHaveWinner = false;
+
+            while (boardCounter < 10)
+            {
+                Console.Clear();
+                board.Print();
+
+                if (boardCounter % 2 == 0)
+                {
+                    var isValidTurn = PlayerTurn(playerTwo, playerTwo.Symbol);
+
+                    if (!isValidTurn)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    var isValidTurn = PlayerTurn(playerOne, playerOne.Symbol);
+
+                    if (!isValidTurn)
+                    {
+                        continue;
+                    }
+                };
+
+                if (boardCounter > 4)
+                {
+                    var (haveWinner, winnerString) = board.ChekForWinner();
+
+                    if (haveWinner)
+                    {
+                        Console.Clear();
+                        board.Print();
+                        var player = playerOne.Symbol == winnerString ? playerOne : playerTwo;
+                        IsHaveWinner = true;
+                        PrintPlayersMessege($"We have winner {player.Name}!!!");
+                        break;
+                    }
+                }
+
+                boardCounter++;
+            }
+
+            if (!IsHaveWinner)
+            {
+                PrintPlayersMessege("DRAW! No one winning!");
+            }
+
+            PrintPlayersMessege("For resrart the game tape Yes");
+
+            var restartGame = Console.ReadLine();
+
+            if (restartGame.ToLower() == "yes")
+            {
+                ConfigurateGame();
+            }
+        }
+
+        public bool PlayerTurn(Player player, string symbol)
+        {
+            PrintPlayersTurnMessage(player);
+            var position = Console.ReadLine();
+            var (isValidTurn, message) = board.ChangeBoard(position, symbol);
+            PrintPlayersMessege(message);
+
+            return isValidTurn;
+        }
+
+        public void PrintPlayersTurnMessage(Player player)
+        {
+            Console.WriteLine($"It is a {player.Name} turn");
+            Console.WriteLine("Chose position:");
+        }
+
+        public void PrintPlayersMessege(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- SeaChess/SeaChess/Program.cs
+++ SeaChess/SeaChess/Program.cs
@@ -5,95 +5,21 @@ namespace SeaChess
     public class Program
     {
 
-        public static Board? board;
+        
         public static void Main()
         {
-            board = new Board();
-
+           ConfigurateGame();
+        }
+        
+        public static void ConfigurateGame()
+        {
             var playerOne = new Player("X", "Player One");
 
             var playerTwo = new Player("O", "Player Two");
 
-            var boardCounter = 1;
+            var game = new Game(playerOne, playerTwo);
 
-            var IsHaveWinner = false;
-
-            while (boardCounter < 10) 
-            {
-                Console.Clear();
-                board.Print();
-
-                if (boardCounter % 2 == 0)
-                {
-                    var isValidTurn = PlayerTurn(playerTwo, playerTwo.Symbol);
-
-                    if (!isValidTurn)
-                    {
-                        continue;
-                    }
-                }
-                else 
-                {
-                    var isValidTurn = PlayerTurn(playerOne, playerOne.Symbol);
-
-                    if (!isValidTurn)
-                    {
-                        continue;
-                    }
-                };   
-                
-                if(boardCounter > 4)
-                {
-                    var (haveWinner, winnerString) = board.ChekForWinner();
-
-                    if (haveWinner) 
-                    {
-                        Console.Clear();
-                        board.Print();
-                        var player = playerOne.Symbol == winnerString ? playerOne : playerTwo;
-                        IsHaveWinner = true;
-                        PrintPlayersMessege($"We have winner {player.Name}!!!");                        
-                        break;
-                    }
-                }
-
-                boardCounter++;                
-            }
-
-            if (!IsHaveWinner)
-            {
-                PrintPlayersMessege("DRAW! No one winning!");
-            }
-
-            PrintPlayersMessege("For resrart the game tape Yes");
-
-            var restartGame = Console.ReadLine();
-
-            if (restartGame.ToLower() == "yes") 
-            {
-                Main();
-            }
-        }
-
-        public static bool PlayerTurn(Player player, string symbol)        
-        {
-            PrintPlayersTurnMessage(player);
-            var position = Console.ReadLine();
-            var (isValidTurn, message) = board.ChangeBoard(position, symbol);
-            PrintPlayersMessege(message);
-
-            return isValidTurn;
-        }
-
-        public static void PrintPlayersTurnMessage(Player player)
-        {
-            Console.WriteLine($"It is a {player.Name} turn");
-            Console.WriteLine("Chose position:");
-        }
-
-        public static void PrintPlayersMessege(string message)
-        {
-            Console.WriteLine(message);
+            game.Run();
         }
     }
 }

8882cdc1a9a548dd3aed5a8c6ba4389c8a4ec75d
Sand82 <sandoki@abv.bg>
2023-09-07 22:12:54 +0300

    added new exercise

---
+++ DesignPatterns/Creational/Prototype/Prototype/Color.cs
@@ -0,0 +1,24 @@
+﻿namespace Prototype
+{
+    public class Color : ColorPrototype
+    {
+        private int red;
+
+        private int green;
+
+        private int blue;
+
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}",red, green, blue);
+            return this.MemberwiseClone() as ColorPrototype;
+        }
+    }
+}

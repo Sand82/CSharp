50a802c35c512451fb361c71147e01777cf1c9c2
Sand82 <sandoki@abv.bg>
2023-12-21 15:44:11 +0200

    added new exercise

---
+++ BasicWebServer/BasicWebServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Text;
+
+namespace Program
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            HttpClient client = new HttpClient();
+
+            await ReadData(client);
+            
+        }
+
+        private static async Task ReadData(HttpClient client)
+        {
+            var url = "https://softuni.bg/";
+
+            var response = await client.GetAsync(url);
+            response.Headers.Add("X-ServerVersion", "1.0.0");
+            response.Headers.Add("X-ServerAuthorUsername", "Sand82");
+
+            var data = String.Join(Environment.NewLine, response.Headers.Select(x => x.Key + ": " + x.Value.First()));
+
+            Console.WriteLine(data);
+        }
+    }
+}

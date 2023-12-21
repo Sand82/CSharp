b48c3dd637e8698861c7dfe46c16c7487949960e
Sand82 <sandoki@abv.bg>
2023-12-21 17:12:03 +0200

    added new exercise

--- BasicWebServer/BasicWebServer/Program.cs
+++ BasicWebServer/BasicWebServer/Program.cs
@@ -1,16 +1,55 @@
 ﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Program
 {
     public class Program
     {
+        private static string newline = "\r\n";
+
         public static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            HttpClient client = new HttpClient();
+            HttpClient httpClient = new HttpClient();
 
-            await ReadData(client);
+            var port = 12345;
+
+            TcpListener TcpListener = new TcpListener(IPAddress.Loopback, port);
+            TcpListener.Start();                      
+
+            while (true) 
+            {
+                var client = TcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    var buffer = new byte[1000000];
+
+                    var byteData = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    var data = Encoding.UTF8.GetString(buffer, 0, byteData);
+
+                    Console.WriteLine(data);
+
+                    Console.WriteLine(new string('=', 60));
+
+                    string html = $"<h1>Hello from Sand {DateTime.Now}</h1>";
+
+                    int htmlLenght = html.Length;
+
+                    string responce = "HTTP/1.1 200 OK" + newline +
+                        "SandServer: 2023" + newline +
+                        "Content-Type: application/json; charSet=utf8" + newline +
+                        "ContentLenght: htmlLenght: " + htmlLenght + newline + newline
+                        + html + newline;
+
+                    byte[] byteResponse = Encoding.UTF8.GetBytes(responce);
+
+                    stream.Write(byteResponse);                   
+                }                
+            }
+
+            //await ReadData(client);
             
         }
 
@@ -22,7 +61,7 @@ namespace Program
             response.Headers.Add("X-ServerVersion", "1.0.0");
             response.Headers.Add("X-ServerAuthorUsername", "Sand82");
 
-            var data = String.Join(Environment.NewLine, response.Headers.Select(x => x.Key + ": " + x.Value.First()));
+            var data = String.Join(Environment.NewLine, response.Headers.Select(x => x.Key + ": " + String.Join("; ",x.Value.Distinct())));
 
             Console.WriteLine(data);
         }

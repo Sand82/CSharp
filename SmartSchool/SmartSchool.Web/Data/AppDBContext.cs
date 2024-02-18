c37f5a21cb31f4127716f8b05a6856e67598fd14
Sand82 <sandoki@abv.bg>
2024-02-18 22:05:18 +0200

    added identity configuration on extender configuration builder class

---
+++ SmartSchool/SmartSchool.Web/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartSchool.Web.Data
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions opt) : base(opt)
+        {
+                
+        }
+    }
+}

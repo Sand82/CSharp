d997cc8cfa5ca7d8b0d49dff55f0661d1233762b
Sand82 <sandoki@abv.bg>
2024-03-01 21:40:34 +0200

    added new Web API project

--- SmartSchool/SmartSchool.AppHost/Program.cs
+++ SmartSchool/SmartSchool.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.SmartSchool_Web>("web");
 
+builder.AddProject<Projects.SmartSchool_Students>("smartschool.students");
+
 builder.Build().Run();

b807fef9aaa18295805d2ff945a4c91e5bb22af8
Sand82 <sandoki@abv.bg>
2024-03-01 23:07:22 +0200

    added model on the Web Api server

---
+++ SmartSchool/SmartSchool.Students/Domain/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartSchool.Students.Domain.Models
+{
+    public class Student
+    {
+        #region Constructors and properties
+        public int Id { get; set; }
+            
+        public required string RollNumber { get; set; }
+        
+        public required string FirstName { get; set; }
+        
+        public required string LastName { get; set; }
+        
+        public required string Email { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public int Age => CalculateAge();
+
+        public string? PhoneNumber { get; set; }
+
+        public Address? Address { get; set; }
+        #endregion
+
+        #region Public behavior
+        #endregion
+
+        #region Private behavior
+        private int CalculateAge()
+        {
+            var currentDate = DateTime.UtcNow;
+            var age = currentDate.Year - DateOfBirth.Year;
+
+            if (currentDate.Month < DateOfBirth.Month || 
+                currentDate.Month == DateOfBirth.Month && currentDate.Day < DateOfBirth.Day)
+            {
+                age--;
+            }
+
+            return age;
+        }
+        #endregion
+    }
+}

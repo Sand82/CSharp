fbe5ea246529faa49a6d15660c427cb6f1b42ff4
Sand82 <sandoki@abv.bg>
2024-03-01 21:46:44 +0200

    added new Web Api project back end from front end

--- SmartSchool/SmartSchool.AppHost/Program.cs
+++ SmartSchool/SmartSchool.AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.SmartSchool_Web>("web");
 
 builder.AddProject<Projects.SmartSchool_Students>("smartschool.students");
 
+builder.AddProject<Projects.SmartSchool_Bff>("smartschool.bff");
+
 builder.Build().Run();

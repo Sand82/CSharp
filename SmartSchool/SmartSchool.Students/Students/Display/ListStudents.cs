55fd6dcf5acd86ef5c9dbef00e94bc27a39effee
Sand82 <sandoki@abv.bg>
2024-03-09 21:59:21 +0200

    added new get all students service

---
+++ SmartSchool/SmartSchool.Students/Students/Display/ListStudents.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.Students.Data;
+
+namespace SmartSchool.Students.Students.Display
+{
+    public record ListStudents(PagingOptions Paging) 
+        : IRequest<IEnumerable<StudentBasicInfo>>;
+
+
+    public class ListStudentsHandler : IRequestHandler<ListStudents, IEnumerable<StudentBasicInfo>>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ListStudentsHandler(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<StudentBasicInfo>> Handle(ListStudents request, CancellationToken cancellationToken)
+        {
+            var itemToSkip = request.Paging.PageSize * (request.Paging.PageSize - 1);
+            var students = await _dbContext.Students
+                .OrderBy(s => s.Id)
+                .Skip(itemToSkip)
+                .Take(request.Paging.PageSize)
+                .Select(s => new StudentBasicInfo(s.Id, s.RollNumber, s.FirstName, s.LastName, s.Email, s.DateOfBirth, s.Age))
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return students;
+
+        }
+    }
+}
---
+++ SmartSchool/SmartSchool.Students/Students/Display/StudentInfoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.Students.Students.Display
+{
+    public class StudentInfoController : StudentsBaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(StudentBasicInfo[]), 200)]
+        public async Task<IActionResult> ListStudents([FromQuery] PagingOptions paging, CancellationToken token)
+        {
+            var result = await Mediator.Send(new ListStudents(paging), token);
+
+            return Ok(result);
+        }
+    }
+}
+
+public record PagingOptions(int PageNumber = 1, int PageSize = 10);

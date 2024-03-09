4d73e34cb52c43d9b3ca98b1cb6bb47358413871
Sand82 <sandoki@abv.bg>
2024-03-09 22:40:20 +0200

    connect bff with web

--- SmartSchool/SmartSchool.Bff/ApiClients/IStudentsApiClient.cs
+++ SmartSchool/SmartSchool.Bff/ApiClients/IStudentsApiClient.cs
@@ -6,8 +6,12 @@ namespace SmartSchool.Bff.ApiClients
     {
         [Post("/students")]
         Task<StudentBaseInfo> CreateStudent([Body] CreateStudent newStudent);
+
+        [Get("/students")]
+        Task<StudentBaseInfo[]> ListStudents([Query] PagingOptions paging);
     }
 }
 
 public record CreateStudent(string RollNumber, string FirstName, string LastName, DateTime DateOfBirth);
 public record StudentBaseInfo(int StudentId, string RoleNumber, string FirstName, string LastName, string Email ,DateTime DateOfBirth, int Age);
+public record PagingOptions(int PageNumber = 1, int PageSize = 10);
--- SmartSchool/SmartSchool.Bff/Students/StudentEndpoints.cs
+++ SmartSchool/SmartSchool.Bff/Students/StudentEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmartSchool.Bff.ApiClients;
+using System.Runtime.InteropServices;
 
 namespace SmartSchool.Bff.Students
 {
@@ -9,19 +10,29 @@ namespace SmartSchool.Bff.Students
         {
             var students = app.MapGroup("/students");            
 
-            students.MapPost("/", async ([FromBody]CreateStudent newStudent, 
-                [FromServices] IStudentsApiClient studentClient) 
-                => await CreateStudent(studentClient, newStudent))
+            students.MapPost("/", async ([FromBody]CreateStudent newStudent, [FromServices] IStudentsApiClient studentClient) 
+                                => await CreateStudent(studentClient, newStudent))
                 .WithName("CreateStudent")
                 .Produces(200, typeof(StudentBaseInfo));
 
+            students.MapGet("/", async(IStudentsApiClient client, int pageNumber, int pageSize)
+                                => await ListStudents(client, pageNumber, pageSize))
+                .WithName("ListStudents")
+                .Produces(200, typeof(StudentBaseInfo[]));
+
             return app;
-        }
+        }        
 
         private static async Task<IResult> CreateStudent(IStudentsApiClient studentsClient, CreateStudent newStudent)
         {
             var result = await studentsClient.CreateStudent(newStudent);
             return TypedResults.Ok(result);
+        }
+
+        private static async Task<IResult> ListStudents(IStudentsApiClient client, int pageNumber, int pageSize)
+        {
+            var result = await client.ListStudents(new PagingOptions(pageNumber, pageSize));
+            return TypedResults.Ok(result);            
         }
     }
 }

2f8eeae52be022b4d08bec5e724a1b0c4bc694c3
Sand82 <sandoki@abv.bg>
2024-05-04 23:48:27 +0300

    added new exercise

---
+++ Exercise/Recursion/BinarySearchRecursive/BinarySearchRecursive/BinarySearchRecursive/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinarySearchRecursive
+{
+    public class Program
+    {
+        private static int[] numbers;
+
+        static void Main(string[] args)
+        {
+            var numbersInput = Console.ReadLine()!
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            numbers = numbersInput.OrderBy(x => x).ToArray();
+
+            var n = int.Parse(Console.ReadLine()!);
+
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            Console.WriteLine(BinarySearch(n, left, right));
+        }
+
+        private static bool BinarySearch(int n, int left, int right)
+        {
+            if (left >= right)
+            {
+                return false;
+            }
+
+            var middle = (left + right) / 2;
+            var element = numbers[middle];
+
+            if (element == n)
+            {
+                return true;
+            }
+
+            if (n > element)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+
+            return BinarySearch(n, left, right);
+        }
+    }
+}

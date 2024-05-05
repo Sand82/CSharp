9b0608a096f4383f70fad092b4f20fed96bfd156
Sand82 <sandoki@abv.bg>
2024-05-05 20:58:14 +0300

    added new exercise

---
+++ Exercise/Recursion/MergeSort/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            var sorted = MergeSort(numbers);
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        private static int[] MergeSort(int[] numbers)
+        {
+            if (numbers.Length <= 1)
+            {
+                return numbers;
+            }
+
+            var left = numbers.Take(numbers.Length / 2).ToArray();
+            var right = numbers.Skip(numbers.Length / 2).ToArray();
+
+            return Merge(MergeSort(left), MergeSort(right));
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            var arr = new int[left.Length + right.Length];
+
+            var mergedIndex = 0;
+            var leftIndex = 0;
+            var rightIndex = 0;
+
+
+            while (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    arr[mergedIndex++] = left[leftIndex++];
+                }
+                else if(left[leftIndex] > right[rightIndex])
+                {
+                    arr[mergedIndex++] = right[rightIndex++];
+                }
+            }
+
+            for (int i = leftIndex; i < left.Length; i++)
+            {
+                arr[mergedIndex++] = left[i];
+            }
+
+            for (int i = rightIndex; i < right.Length; i++)
+            {
+                arr[mergedIndex++] = right[i];
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Exercise/Recursion/QuickSort/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()!.Split(" ").Select(int.Parse).ToList();
+
+            QuickSort(numbers, 0, numbers.Count - 1);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void QuickSort(List<int> numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var pivot = start;
+            var left = start + 1;
+            var right = end;
+
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot] && numbers[right] < numbers[pivot])
+                {
+                    Swap(numbers, left, right);
+                }
+
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left += 1;
+                }
+
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right -= 1;
+                }
+            }            
+
+            Swap(numbers, pivot, right);
+
+            QuickSort(numbers, start, right - 1);
+            QuickSort(numbers, right + 1, end);
+        }
+
+        private static void Swap(List<int> numbers, int left, int right)
+        {
+            var temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+        }
+    }
+}

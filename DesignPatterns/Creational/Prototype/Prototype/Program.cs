1792c1090b413c26e99b12ccb9aa307e1aaa307f
Sand82 <sandoki@abv.bg>
2026-03-31 22:39:11 +0300

    added new exercise

--- DesignPatterns/Creational/Prototype/Prototype/Program.cs
+++ DesignPatterns/Creational/Prototype/Prototype/Program.cs
@@ -1,22 +1,13 @@
-﻿namespace Prototype
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            ColorManager colormanager = new ColorManager();
-            
-            colormanager["red"] = new Color(255, 0, 0);
-            colormanager["green"] = new Color(0, 255, 0);
-            colormanager["blue"] = new Color(0, 0, 255);
-            
-            colormanager["angry"] = new Color(255, 54, 0);
-            colormanager["peace"] = new Color(128, 211, 128);
-            colormanager["flame"] = new Color(211, 34, 20);
-            
-            Color color1 = colormanager["red"].Clone() as Color;
-            Color color2 = colormanager["peace"].Clone() as Color;
-            Color color3 = colormanager["flame"].Clone() as Color;
-        }
-    }
-}
+﻿using Prototype;
+
+Report original = new Report("Monthly Report", "Sales data...");
+
+Report copy1 = (Report)original.Clone();
+copy1.Title = "Monthly Report - Copy 1";
+
+Report copy2 = (Report)original.Clone();
+copy2.Title = "Monthly Report - Copy 2";
+
+original.Show();
+copy1.Show();
+copy2.Show();
---
+++ DesignPatterns/Creational/Prototype/Prototype/Report.cs
@@ -0,0 +1,25 @@
+﻿namespace Prototype;
+
+public class Report : IPrototype
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public Report(string title, string content)
+    {
+        Title = title;
+        Content = content;
+    }
+
+    public IPrototype Clone()
+    {
+        return (IPrototype)this.MemberwiseClone();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Content: {Content}");
+        Console.WriteLine("-------------------------");
+    }
+}

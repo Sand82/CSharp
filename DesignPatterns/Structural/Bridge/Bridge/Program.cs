cfc6f4f4e2c3f830f76a7fbc2c1971904fb3b538
Sand82 <sandoki@abv.bg>
2026-04-05 13:16:13 +0300

    added advanced program logic

--- DesignPatterns/Structural/Bridge/Bridge/Program.cs
+++ DesignPatterns/Structural/Bridge/Bridge/Program.cs
@@ -1,16 +1,54 @@
 ﻿using Bridge;
-using System.Net.Http.Headers;
 
-var device = new TV();
-var remoteControl = new RemoteControl(device);
+IDevice device;
 
-remoteControl.TogglePower();
-remoteControl.VolumeUp();
+while (true)
+{
+    Console.WriteLine("Create TV or Radio Device");
 
-Console.WriteLine("----------------------------");
+    var input = Console.ReadLine();
 
-IDevice radio = new Radio();
-AdvancedRemoteControl advancedRemote = new AdvancedRemoteControl(radio);
+    if (input?.ToLower() == "tv")
+    {
+        device = new TV();
+    }
+    else if (input?.ToLower() == "radio")
+    {
+        device = new Radio();
+    }
+    else
+    {
+        break;
+    }
 
-advancedRemote.TogglePower();
-advancedRemote.Mute();
+    Console.WriteLine("Use remote device command Toggle Power, Volume Up, Volume Down or Mute");
+
+    var advancedRemoteDevice = new AdvancedRemoteControl(device);
+
+    while (true)
+    {
+        var deviceCommand = Console.ReadLine();
+
+        if (deviceCommand?.ToLower() == "toggle power")
+        {
+            advancedRemoteDevice.TogglePower();
+        }
+        else if (deviceCommand?.ToLower() == "volume up")
+        {
+            advancedRemoteDevice.VolumeUp();
+        }
+        else if (deviceCommand?.ToLower() == "volume down")
+        {
+            advancedRemoteDevice.VolumeDown();
+        }
+        else if (deviceCommand?.ToLower() == "mute")
+        {
+            advancedRemoteDevice.Mute();
+        }
+        else
+        {
+            Console.WriteLine("Do you want to use another device");
+            break;
+        }
+    }
+}

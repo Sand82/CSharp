8a83588c6fc67ce992770be5ef712da927827fc3
Sand82 <sandoki@abv.bg>
2026-05-02 10:49:36 +0300

    remove folder

---
+++ DesignPatterns/Structural/Adapter/Adapter/OldPrinter.cs
@@ -0,0 +1,9 @@
+﻿namespace Adapter;
+
+public class OldPrinter
+{
+    public void PrintText(string text)
+    {
+        Console.WriteLine("Old printer print: " + text);
+    }
+}
---
+++ DesignPatterns/Structural/Adapter/Adapter/Program.cs
@@ -0,0 +1,7 @@
+﻿using Adapter;
+
+var oldPrinter = new OldPrinter();
+
+var printer = new AdapterPrinter(oldPrinter);
+
+printer.PrintText("Hello Adapter Pattern in .NET!");

6dba895aaadcb082edad4dc251a7c336fcb7a51b
Sand82 <sandoki@abv.bg>
2026-05-07 11:06:31 +0300

    added files

---
+++ DesignPatterns/Structural/Proxy/Proxy/ProxiImage.cs
@@ -0,0 +1,22 @@
+﻿namespace Proxy;
+
+public class ProxiImage : IImage
+{
+    private IImage? realImage;
+    private string? fileName;
+
+    public ProxiImage(string? fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void Display()
+    {
+        if (realImage == null)
+        {
+            realImage = new RealImage(fileName);
+        }
+
+        realImage.Display();
+    }
+}
---
+++ DesignPatterns/Structural/Proxy/Proxy/RealImage.cs
@@ -0,0 +1,22 @@
+﻿namespace Proxy;
+
+public class RealImage : IImage
+{
+    private string? fileName;
+
+    public RealImage(string? fileName)
+    {
+        this.fileName = fileName;
+        LoadFormDisk();
+    }
+
+    private void LoadFormDisk()
+    {
+        Console.WriteLine("Loading image from disk " + fileName);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Display image " + fileName);
+    }
+}

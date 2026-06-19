4b09825d0f01d706b4757ca79a27525feb0bb73c
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2026-06-19 17:04:07 +0300

    Refactor program method

--- DesignPatterns/Behavioral/Command/Command/Program.cs
+++ DesignPatterns/Behavioral/Command/Command/Program.cs
@@ -6,46 +6,25 @@ bool isLightOn = false;
 
 while (true)
 {
-    var isOn = isLightOn  ? "On" : "Off";
+    var isOn = isLightOn ? "On" : "Off";
     Console.WriteLine("Do you want to lighs " + isOn + " - Yes/No");
     var input = Console.ReadLine();
     ICommand command = null;
 
-    if (input!.ToLower() != "yes" || input!.ToLower() != "no")
-    {
-        continue;
-    }
-
     if (input!.ToLower() == "no")
     {
         break;
+    }
+
+    if (input!.ToLower() != "yes")
+    {
+        continue;
     }    
 
-    if (isLightOn == false)
-    {        
-        command = new TurnOnLightCommand(light);
-    }
-
-    if (isLightOn == true)
-    {       
-        command = new TurnOffLightCommand(light);
-    }
+    command = isLightOn ? new TurnOffLightCommand(light) : new TurnOnLightCommand(light);    
 
     isLightOn = !isLightOn;
     var remote = new RemoteControl();
     remote.SetCommand(command);
     remote.PressButton();
 }
-
-
-
-//var turnOn = new TurnOnLightCommand(light);
-//var turnOff = new TurnOffLightCommand(light);
-
-//var remote = new RemoteControl();
-
-//remote.SetCommand(turnOn);
-//remote.PressButton();
-
-//remote.SetCommand(turnOff);
-//remote.PressButton();

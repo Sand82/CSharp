7d2281a38ab1eff757227849ff75405f359786c5
Sand82 <sandoki@abv.bg>
2026-07-19 17:01:20 +0300

    added new predicate exercise

---
+++ Delegates/Predicate/Predicate/LengthChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace Predicate
+{
+    public class LengthChecker
+    {
+        private string? stringToCheck;
+        
+        public LengthChecker(string? stringToCheck)
+        {
+            this.stringToCheck = stringToCheck;
+            IntToCheck = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+        }
+
+        public List<int> IntToCheck { get; set; }
+
+        public bool IsStringLong( Predicate<string> stringChecker) 
+            => stringChecker(stringToCheck!);        
+
+        public bool HasStringWantedCharacter(Predicate<char> stringChecker, char character)
+            => stringChecker(character);
+
+        public bool HasWantedNumber(Predicate<int> stringChecker, int number) 
+            => stringChecker(number);
+
+        public string GetString()
+        {
+            return stringToCheck!;
+        }
+    }
+}
---
+++ Delegates/Predicate/Predicate/Program.cs
@@ -0,0 +1,23 @@
+﻿using Predicate;
+
+var stringChecker = new LengthChecker("Make it simple");
+
+var result = stringChecker
+    .IsStringLong(new Predicate<string>(x => x.Length > 5));
+
+var hasTheStringCharacter = stringChecker
+    .HasStringWantedCharacter(x => stringChecker.GetString().Contains(x), 'A');
+
+var number = 10;
+var hasWantedNumber = stringChecker
+    .HasWantedNumber(x => HasNumber(stringChecker.IntToCheck, number), number);
+
+Console.WriteLine(result);
+Console.WriteLine(hasTheStringCharacter);
+Console.WriteLine(hasWantedNumber);
+
+bool HasNumber(List<int> numbers, int number)
+{
+    var result = numbers.Any(x => x == number);
+    return result;
+}

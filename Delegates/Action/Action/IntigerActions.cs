0a9dddda1e368c010d6312a47dfff2f13738d6e1
Sand82 <sandoki@abv.bg>
2026-07-19 17:41:50 +0300

    added new Predicate Action exercise

---
+++ Delegates/Action/Action/IntigerActions.cs
@@ -0,0 +1,13 @@
+﻿namespace Action;
+
+public class IntegerActions
+{
+    public void PrintNumberCount(Action<int, List<int>> action, int number, List<int> collection)
+        => action(number, collection);
+
+    public void PrintNumber(Action<int, List<int>> action, int number, List<int> collection)
+        => action(number, collection);
+
+    public void SumArray(Action<List<int>> action, List<int> collection) 
+        => action(collection);
+}
---
+++ Delegates/Action/Action/Program.cs
@@ -0,0 +1,37 @@
+﻿using Action;
+
+var collection = new List<int>() { 1, 1, 3, 3, 5, 6, 7, 3, 9, 2 };
+var integerAction = new IntegerActions();
+var number = 3;
+
+integerAction.PrintNumberCount((x, col) => PrintNumberCounts(x, col), number, collection);
+integerAction.PrintNumber((x, col) => OddNumbers(x, col), number, collection);
+integerAction.PrintNumber((x, col) => EvenNumbers(x, col), number, collection);
+integerAction.SumArray((col) => SumNumbers(col), collection);
+
+void PrintNumberCounts(int number, List<int> numbers)
+{
+    var count = numbers.Where(x => x == number).Count();
+
+    Console.WriteLine(count);
+}
+
+void OddNumbers(int number, List<int> numbers)
+{
+    var coll = numbers.Where( x => x % 2 != 0).ToList();
+
+    Console.WriteLine(string.Join(", ", coll));
+}
+
+void EvenNumbers(int number, List<int> numbers)
+{
+    var coll = numbers.Where(x => x % 2 == 0).ToList();
+
+    Console.WriteLine(string.Join(", ", coll));
+}
+
+void SumNumbers(List<int> numbers)
+{
+    var result = numbers.Sum();
+    Console.WriteLine(result);
+}

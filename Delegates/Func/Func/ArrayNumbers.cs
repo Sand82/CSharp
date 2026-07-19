ae061952d7804e359d403e4d2959ace4781de4cd
Sand82 <sandoki@abv.bg>
2026-07-19 18:11:32 +0300

    added new Delegate Func

---
+++ Delegates/Func/Func/ArrayNumbers.cs
@@ -0,0 +1,9 @@
+﻿namespace Func;
+
+public class ArrayNumbers
+{
+    private readonly IEnumerable<int> collection = new List<int> { 15, 2, 3, 10 , 22 ,17, 36, 58, 29, 78, 20, 15 };
+
+    public IEnumerable<int> NumbersManipulation(Func<IEnumerable<int>, IEnumerable<int>> func)
+        => func(collection);
+}
---
+++ Delegates/Func/Func/Program.cs
@@ -0,0 +1,27 @@
+﻿using Func;
+using System.Linq;
+
+var arrayNumbers = new ArrayNumbers();
+
+ var sortedDescendingColl = arrayNumbers
+    .NumbersManipulation(x => x.OrderByDescending(e => e).ToList());
+
+var sortedAscendingColl = arrayNumbers
+    .NumbersManipulation(x => x.OrderBy(e => e).ToList());
+
+var oddNumbers = arrayNumbers
+    .NumbersManipulation(x => x.Where(y => y % 2 != 0).ToList());
+
+var evenNumbers = arrayNumbers
+    .NumbersManipulation(x => x.Where(y => y % 2 == 0).ToList());
+
+var number = 15;
+
+var numberCount = arrayNumbers
+    .NumbersManipulation(x => x.Where(y => y == number).ToList());
+
+Console.WriteLine(string.Join(", ", sortedDescendingColl));
+Console.WriteLine(string.Join(", ", sortedAscendingColl));
+Console.WriteLine(string.Join(", ", oddNumbers));
+Console.WriteLine(string.Join(", ", evenNumbers));
+Console.WriteLine(numberCount.Count());
